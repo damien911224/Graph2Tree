72b4386d12136ecb8bb0afc137112d643d81fe5d
damien911224 <damien911224@gmail.com>
2021-06-14 20:19:21 +0900

    Modified

--- mawps/src/train_and_evaluate.py
+++ mawps/src/train_and_evaluate.py
@@ -367,7 +367,7 @@ def get_all_number_encoder_outputs(encoder_outputs, num_pos, batch_size, num_siz
         indices += [0 for _ in range(len(num_pos[b]), num_size)]
         masked_index += [temp_1 for _ in range(len(num_pos[b]), num_size)]
     indices = torch.LongTensor(indices)
-    masked_index = torch.ByteTensor(masked_index)
+    masked_index = torch.BoolTensor(masked_index)
     masked_index = masked_index.view(batch_size, num_size, hidden_size)
     if USE_CUDA:
         indices = indices.cuda()
