5a8e6f273b16e063b87448f69a8cd70b515e6c97
damien911224 <damien911224@gmail.com>
2021-06-15 02:14:25 +0900

    Modified

--- mawps2code/cross_valid_mawps.py
+++ mawps2code/cross_valid_mawps.py
@@ -277,31 +277,31 @@ for fold in range(num_folds):
 
         start = time.time()
 
-        # train_loss_total = 0
-        # input_batches, input_lengths, output_batches, output_lengths, nums_batches, \
-        # num_stack_batches, num_pos_batches, num_size_batches, \
-        # num_value_batches, graph_batches = prepare_train_batch(train_pairs, batch_size)
-        # for idx in range(len(input_lengths)):
-        #     train_loss = train_tree(
-        #         input_batches[idx], input_lengths[idx], output_batches[idx], output_lengths[idx],
-        #         num_stack_batches[idx], num_size_batches[idx], generate_num_ids, encoder, decoder, attention_decoder,
-        #         encoder_optimizer, decoder_optimizer, attention_decoder_optimizer,
-        #         output_lang, num_pos_batches[idx], graph_batches[idx])
-        #     train_loss_total += train_loss.detach().cpu().numpy()
-        # train_loss_total = train_loss_total / len(input_lengths)
-        #
-        # val_loss_total = 0
-        # input_batches, input_lengths, output_batches, output_lengths, nums_batches, \
-        # num_stack_batches, num_pos_batches, num_size_batches, \
-        # num_value_batches, graph_batches = prepare_train_batch(test_pairs, batch_size)
-        # for idx in range(len(input_lengths)):
-        #     val_loss = val_tree(
-        #         input_batches[idx], input_lengths[idx], output_batches[idx], output_lengths[idx],
-        #         num_stack_batches[idx], num_size_batches[idx], generate_num_ids, encoder, decoder, attention_decoder,
-        #         encoder_optimizer, decoder_optimizer, attention_decoder_optimizer,
-        #         output_lang, num_pos_batches[idx], graph_batches[idx])
-        #     val_loss_total += val_loss.detach().cpu().numpy()
-        # val_loss_total = val_loss_total / len(input_lengths)
+        train_loss_total = 0
+        input_batches, input_lengths, output_batches, output_lengths, nums_batches, \
+        num_stack_batches, num_pos_batches, num_size_batches, \
+        num_value_batches, graph_batches = prepare_train_batch(train_pairs, batch_size)
+        for idx in range(len(input_lengths)):
+            train_loss = train_tree(
+                input_batches[idx], input_lengths[idx], output_batches[idx], output_lengths[idx],
+                num_stack_batches[idx], num_size_batches[idx], generate_num_ids, encoder, decoder, attention_decoder,
+                encoder_optimizer, decoder_optimizer, attention_decoder_optimizer,
+                output_lang, num_pos_batches[idx], graph_batches[idx])
+            train_loss_total += train_loss.detach().cpu().numpy()
+        train_loss_total = train_loss_total / len(input_lengths)
+
+        val_loss_total = 0
+        input_batches, input_lengths, output_batches, output_lengths, nums_batches, \
+        num_stack_batches, num_pos_batches, num_size_batches, \
+        num_value_batches, graph_batches = prepare_train_batch(test_pairs, batch_size)
+        for idx in range(len(input_lengths)):
+            val_loss = val_tree(
+                input_batches[idx], input_lengths[idx], output_batches[idx], output_lengths[idx],
+                num_stack_batches[idx], num_size_batches[idx], generate_num_ids, encoder, decoder, attention_decoder,
+                encoder_optimizer, decoder_optimizer, attention_decoder_optimizer,
+                output_lang, num_pos_batches[idx], graph_batches[idx])
+            val_loss_total += val_loss.detach().cpu().numpy()
+        val_loss_total = val_loss_total / len(input_lengths)
 
         reference_list = list()
         candidate_list = list()
@@ -309,13 +309,13 @@ for fold in range(num_folds):
         for test_batch in test_pairs:
             #print(test_batch)
             batch_graph = get_single_example_graph(test_batch[0], test_batch[1], test_batch[7], test_batch[4], test_batch[5])
-            # test_res = evaluate_tree(test_batch[0], test_batch[1], generate_num_ids, encoder, decoder, attention_decoder,
-            #                          output_lang, test_batch[5], batch_graph, beam_size=beam_size)
-            test_res = evaluate_tree_ensemble(test_batch[0], test_batch[1], generate_num_ids,
-                                              [encoder for _ in range(3)],
-                                              [decoder for _ in range(3)],
-                                              [attention_decoder for _ in range(3)],
-                                              output_lang, test_batch[5], batch_graph, beam_size=beam_size)
+            test_res = evaluate_tree(test_batch[0], test_batch[1], generate_num_ids, encoder, decoder, attention_decoder,
+                                     output_lang, test_batch[5], batch_graph, beam_size=beam_size)
+            # test_res = evaluate_tree_ensemble(test_batch[0], test_batch[1], generate_num_ids,
+            #                                   [encoder for _ in range(3)],
+            #                                   [decoder for _ in range(3)],
+            #                                   [attention_decoder for _ in range(3)],
+            #                                   output_lang, test_batch[5], batch_graph, beam_size=beam_size)
             # test_res = evaluate_beam_tree(test_batch[0], test_batch[1], generate_num_ids, encoder, decoder,
             #                               attention_decoder,
             #                               output_lang, test_batch[5], batch_graph, beam_size=beam_size)
