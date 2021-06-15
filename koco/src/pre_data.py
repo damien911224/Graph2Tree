532e1c3c121276777f4e347ac4d43ca6f7e01623
damien911224 <damien911224@gmail.com>
2021-06-15 21:33:37 +0900

    Modified

--- koco/src/pre_data.py
+++ koco/src/pre_data.py
@@ -166,6 +166,49 @@ class OutputLang:
     #     for i, j in enumerate(self.index2word):
     #         self.word2index[j] = i
 
+class Tree():
+    def __init__(self):
+        self.parent = None
+        self.num_children = 0
+        self.children = []
+
+    def __str__(self, level=0):
+        ret = ""
+        for child in self.children:
+            if isinstance(child, type(self)):
+                ret += child.__str__(level + 1)
+            else:
+                ret += "\t" * level + str(child) + "\n"
+        return ret
+
+    def add_child(self, c):
+        if isinstance(c, type(self)):
+            c.parent = self
+        self.children.append(c)
+        self.num_children = self.num_children + 1
+
+    def to_string(self):
+        r_list = []
+        for i in range(self.num_children):
+            if isinstance(self.children[i], Tree):
+                r_list.append("( " + self.children[i].to_string() + " )")
+            else:
+                r_list.append(str(self.children[i]))
+        return "".join(r_list)
+
+    def flatten(self, output_lang):
+        r_list = []
+        for i in range(self.num_children):
+            if isinstance(self.children[i], type(self)):
+                r_list.append(output_lang.word2index["<IS>"])
+                cl = self.children[i].flatten(output_lang)
+                for k in range(len(cl)):
+                    r_list.append(cl[k])
+                r_list.append(output_lang.word2index["<IE>"])
+            else:
+                r_list.append(self.children[i])
+        return r_list
+
 def load_raw_data(filename):  # load the json data to list(dict()) for MATH 23K
     print("Reading lines...")
     f = open(filename, encoding="utf-8")
