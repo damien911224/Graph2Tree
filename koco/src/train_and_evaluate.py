a1b58b7397a30ea6f90ed2ae1ebb4d392634de28
damien911224 <damien911224@gmail.com>
2021-06-15 17:58:30 +0900

    Modified

--- koco/src/train_and_evaluate.py
+++ koco/src/train_and_evaluate.py
@@ -933,6 +933,7 @@ def train_tree(input_batch, input_length, target_batch, target_length, nums_stac
 
     encoder_outputs, problem_output, graph_embedding, attention_inputs = \
         encoder(embedded, input_length, orig_idx, batch_graph)
+    exit()
 
     # ===============changed=================
     # sequence mask for attention
