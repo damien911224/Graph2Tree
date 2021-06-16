f93b76de97ed43e16037146b0adda2c73a7c8bf8
damien911224 <damien911224@gmail.com>
2021-06-17 03:25:35 +0900

    Modified

--- koco_kept2/cross_valid_mawps.py
+++ koco_kept2/cross_valid_mawps.py
@@ -390,7 +390,8 @@ for fold in target_folds:
         candidate_list = list()
         bleu_scores = list()
         if epoch <= n_epochs // 4:
-            test_pairs = test_pairs[:10]
+            sample_population = int(round(len(test_pairs) * 0.1))
+            test_pairs = random.sample(test_pairs, sample_population)
         for test_batch in test_pairs:
             batch_graph = get_single_example_graph(test_batch[0], test_batch[1],
                                                    test_batch[7], test_batch[4], test_batch[5])
