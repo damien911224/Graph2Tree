4befa8aa4d2e051d5cc639015b572564a3225438
damien911224 <damien911224@gmail.com>
2021-06-17 14:42:33 +0900

    Modified

--- koco_kept2/cross_valid_mawps.py
+++ koco_kept2/cross_valid_mawps.py
@@ -24,7 +24,7 @@ def read_json(path):
     return file
 
 
-batch_size = 256
+batch_size = 64
 # embedding_size = 128
 # ===============changed=================
 embedding_size = 768
@@ -58,7 +58,7 @@ opt = {
     # "pretrained_bert_path": './electra_model'
 }
 
-log_path = "logs/{}".format("NoSepAtt_Max_B256_F10")
+log_path = "logs/{}".format("NoSepAtt_Max_B64_F10")
 num_folds = 10
 # target_folds = [0, 1, 2, 3, 4]
 target_folds = list(range(num_folds))
