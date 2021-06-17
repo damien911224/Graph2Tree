6fd61fa06972f0e6e4b50971040e16a78774a4f6
damien911224 <damien911224@gmail.com>
2021-06-17 18:00:57 +0900

    Modified

--- koco_kept2/src/train_and_evaluate.py
+++ koco_kept2/src/train_and_evaluate.py
@@ -867,7 +867,7 @@ def recursive_solve(encoder_outputs, graph_embedding, attention_inputs,
                 gt = gt.cuda()
             this_loss = criterion(pred, gt)
             loss += this_loss
-            print(torch.max(pred), torch.max(gt), this_loss)
+            print(torch.argmax(pred[0]), torch.argmax(gt[0]), this_loss)
         cur_index = cur_index + 1
 
     return loss
